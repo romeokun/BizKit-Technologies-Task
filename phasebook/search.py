92078458d2ca893421a9b9c10d3c9c4fb23d95ec
romeokun <jromevan01@gmail.com>
2024-07-05 16:22:19 +0800

    initial implementaion for search users

--- phasebook/search.py
+++ phasebook/search.py
@@ -27,4 +27,28 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    
+    if(len(args) <= 0):
+        return USERS
+    
+    PARAMS = ["id", "name", "age", "occupation"]
+    result = [[] for x in range(4)]
+
+    for user in USERS:
+        print(user)
+        for i in range(len(PARAMS)):
+            if (PARAMS[i]) not in args:
+                continue
+            if i == 0 and (user.get(PARAMS[i]) == args.get(PARAMS[i])):
+                result[i].append(user)
+                break
+
+            if (i == 1 or i == 3) and args.get(PARAMS[i]).lower() in user.get(PARAMS[i]).lower():
+                result[i].append(user)
+                break
+            
+            if i == 2 and int(args.get(PARAMS[i])) >= int(user.get(PARAMS[i]))-1 and int(args.get(PARAMS[i])) <= int(user.get(PARAMS[i]))+1: 
+                result[i].append(user)
+                break
+
+    return [x for items in result for x in items]
